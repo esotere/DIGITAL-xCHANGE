a72cd706ad0615ed37627845ec2f7023d44b1d38
esotere <34551004+esotere@users.noreply.github.com>
2021-08-31 23:52:13 -0400

    made changes to server modded conditional statement for heroku issues

--- server.jsx
+++ server.jsx
@@ -49,7 +49,7 @@ app.set("view engine", "ejs");
 if(process.env.NODE_ENV === 'production') {
    app.use(express.static("build"));
       app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+        res.sendFile(path.resolve(__dirname,  "public", "server.jsx"));
   });
   
 }
