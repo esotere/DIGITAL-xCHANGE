7509341bc19912ad7d68b3ac00a98ba94e3bf716
esotere <34551004+esotere@users.noreply.github.com>
2021-08-31 22:48:15 -0400

    made changes to server.jsx changed client/build back for heroku issues

--- server.jsx
+++ server.jsx
@@ -47,7 +47,7 @@ app.use(expressLayouts);     // +==> must be above app.set or won't work
 app.set("view engine", "ejs");
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'views', 'build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'DIGITAL_XCHANGE', 'build', 'server.jsx'))
